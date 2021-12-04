c20234bcca5cce0d5bf4582516ef5c7988dbece8
zsfen <zhangximgdao@gmail.com>
2021-12-04 09:55:12 +0800

    mongodb bug

--- src/server/DBOP/doc_add.ts
+++ src/server/DBOP/doc_add.ts
@@ -1,18 +1,23 @@
-import { operateCollection } from "@/server/server";
+import { connectDataBase, operateCollection } from "@/server/server";
 import { Document } from "bson";
 const addObject = (data: Document) => {
   return new Promise((resolve, reject) => {
     if (operateCollection !== null) {
-      operateCollection.insertOne(data, {}, function (err, result) {
-        if (!err) {
-          resolve(result);
-        }
-        reject(err);
-      });
+      addCore(operateCollection, data, resolve, reject);
     } else {
-      reject("没有选中集合!");
+      connectDataBase().then((res) => {
+        addCore(operateCollection, data, resolve, reject);
+      });
     }
   });
 };
 
+const addCore = (operateCollection, data, resolve, reject) => {
+  operateCollection.insertOne(data, {}, function (err, result) {
+    if (!err) {
+      resolve(result);
+    }
+    reject(err);
+  });
+};
 export { addObject };
